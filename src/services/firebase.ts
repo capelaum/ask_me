36fce002cca56711f216932485d999b9b00e1e18
Luís Vinicius Capelletto <thecapellett@gmail.com>
2021-06-24 23:03:34 -0300

    fix typo

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,7 +1,7 @@
-import firebase from "firebase";
+import firebase from "firebase/app";
 
-import "firebse/auth"
-import "firebse/database"
+import "firebase/auth"
+import "firebase/database"
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
