b7a6834159ed6dd4e6ff38e3a4d233262c293ba7
Luís Vinicius Capelletto <thecapellett@gmail.com>
2021-06-26 13:48:56 -0300

    feat: recovering auth state

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import { Route, BrowserRouter } from "react-router-dom";
 import { auth, firebase } from "./services/firebase";
 
@@ -20,6 +20,24 @@ export const AuthContext = createContext({} as AuthContextType);
 
 function App() {
   const [user, setUser] = useState<User>();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        const { displayName, photoURL, uid } = user;
+
+        if (!displayName || !photoURL) {
+          throw new Error("Missing ingormation from Google account.");
+        }
+
+        setUser({
+          id: uid,
+          name: displayName,
+          avatar: photoURL,
+        });
+      }
+    });
+  }, []);
 
   async function signInWithGoogle() {
     const provider = new firebase.auth.GoogleAuthProvider();
