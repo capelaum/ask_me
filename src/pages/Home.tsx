5e4fddfdbb552181f65efc3f6f2dc161d93e78b5
Luís Vinicius Capelletto <thecapellett@gmail.com>
2021-06-26 23:07:21 -0300

    feat: change styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -25,8 +25,10 @@ export function Home() {
     <div id='page-auth'>
       <aside>
         <img src={illustrationSvg} alt='Ilustração de perguntas e respostas' />
-        <strong>Crie salas de Q&amp;A ao-vivo</strong>
-        <p>Tire as dúvidas da sua audiência em tempo-real</p>
+        <div className='aside-text'>
+          <strong>Crie salas de Q&amp;A ao vivo</strong>
+          <p>Tire as dúvidas da sua audiência em tempo-real</p>
+        </div>
       </aside>
       <main>
         <div className='main-content'>
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -14,8 +14,10 @@ export function NewRoom() {
     <div id='page-auth'>
       <aside>
         <img src={illustrationSvg} alt='Ilustração de perguntas e respostas' />
-        <strong>Crie salas de Q&amp;A ao-vivo</strong>
-        <p>Tire as dúvidas da sua audiência em tempo-real</p>
+        <div className='aside-text'>
+          <strong>Crie salas de Q&amp;A ao vivo</strong>
+          <p>Tire as dúvidas da sua audiência em tempo-real</p>
+        </div>
       </aside>
       <main>
         <div className='main-content'>
