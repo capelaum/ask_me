333789f3e1768ea22fd688fe7a215c0281eeae60
Luís Vinicius Capelletto <thecapellett@gmail.com>
2021-06-28 14:49:24 -0300

    styles: Room page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -30,7 +30,7 @@ export function Home() {
     const roomRef = await database.ref(`rooms/${roomCode}`).get();
 
     if (!roomRef.exists()) {
-      alert("Room does not exists");
+      alert("Sala não existe 😕");
       return;
     }
 
--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,7 +1,35 @@
+import logoSvg from "../assets/images/logo.svg";
+import { Button } from "../components/Button";
+
+import "../styles/room.scss";
+
 export function Room() {
   return (
-    <>
-      <h1>Room Page</h1>
-    </>
+    <div id='page-room'>
+      <header>
+        <div className='content'>
+          <img src={logoSvg} alt='ask_me' />
+          <div>codigo</div>
+        </div>
+      </header>
+
+      <main>
+        <div className='room-title'>
+          <h1>Sala React</h1>
+          <span>4 perguntas</span>
+        </div>
+
+        <form action=''>
+          <textarea placeholder='O que você quer perguntar?' />
+
+          <div className='form-footer'>
+            <span>
+              Para enviar uma pergunta, <button>faça seu login</button>
+            </span>
+            <Button type='submit'>Enviar uma pergunta</Button>
+          </div>
+        </form>
+      </main>
+    </div>
   );
 }
